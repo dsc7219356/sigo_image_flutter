108135a6088ca692d18502f207850408193f1805
dsc <549252873@qq.com>
2023-05-31 15:33:48 +0800

    修改bug

--- example/android/app/src/main/java/com/example/sigo_image_flutter_example/sigo_image_loader/SigoImageFileLoader.java
+++ example/android/app/src/main/java/com/example/sigo_image_flutter_example/sigo_image_loader/SigoImageFileLoader.java
@@ -18,7 +18,6 @@ import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.request.transition.Transition;
 import com.example.sigo_image_flutter.loader.SigoImageLoaderProtocol;
-import com.example.sigo_image_flutter.loader.SigoImageLoaderProtocol;
 import com.example.sigo_image_flutter.loader.SigoImageResult;
 import com.example.sigo_image_flutter.request.SigoImageRequestConfig;
 
